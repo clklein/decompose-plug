99e306c8ae4300c91db85fe0ba9c08c6b4b54c87
Casey Klein <clklein@racket-lang.org>
2011-05-18 10:27:47 -0500

    Fixes typo that disabled set-comp tests

--- run-tests.rkt
+++ run-tests.rkt
@@ -3,5 +3,5 @@
 (require "non-syntax-directed-match-test.rkt"
          "syntax-directed-match-test.rkt"
          "syntax-directed-match-total-test.rkt"
-         "set-comp.rkt"
+         "set-comp-test.rkt"
          "reduction-test.rkt")
