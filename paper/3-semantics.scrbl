0193f3097f13f46e7d549f4b9d6667f7f70d6360
Casey Klein <clklein@racket-lang.org>
2011-05-23 13:50:49 -0500

    Changes the notation for matching and decomposition
    Hopefully this makes it easier to remember which pieces are which.

--- paper/3-semantics.scrbl
+++ paper/3-semantics.scrbl
@@ -2,14 +2,28 @@
 
 @(require scriblib/figure
           redex/pict
+          slideshow/pict
           "typesetting-setup.rkt"
+          "../sem-sem/patterns.rkt"
           "../sem-sem/non-syntax-directed-match-define-relation.rkt")
 
 @title{A Semantics for Matching}
 
 @figure["matching" "Matching and Decomposition"]{
-  @centered{
-    @(with-rewriters (render-relation matches))
-    @(with-rewriters (render-relation decomposes))
-  }
+  @(centered
+    (with-rewriters
+     (let ([matches-schema (rule-schema patterns (matches L t p b))]
+           [matches-rules (render-relation matches)]
+           [decomposes-schema (rule-schema patterns (decomposes L t C t p b))]
+           [decomposes-rules (render-relation decomposes)]
+           [vertical-space 10])
+       (pin-over
+        (pin-over
+         (vc-append (+ (pict-height decomposes-schema) (* 2 vertical-space))
+                    matches-rules 
+                    decomposes-rules)
+         0 0 
+         matches-schema)
+        0 (+ (pict-height matches-rules) vertical-space) 
+        decomposes-schema))))
 }
--- paper/typesetting-setup.rkt
+++ paper/typesetting-setup.rkt
@@ -1,27 +1,65 @@
 #lang racket/base
-(require redex/pict)
-(provide with-rewriters)
+(require redex/pict
+         slideshow/pict)
+(provide with-rewriters
+         rule-schema)
+
 (define-syntax-rule 
   (with-rewriters arg)
   (with-rewriters/proc (λ () arg)))
 
+(define-syntax-rule
+  (rule-schema language schema)
+  (frame (inset (render-lw language (to-lw schema)) 3 3)))
+
 (define (with-rewriters/proc thunk)
   (with-compound-rewriter
-   'append-contexts
-   (λ (lws) 
-     (list ""
-           (list-ref lws 2)
-           " ++ "
-           (list-ref lws 3)
-           " = "
-           (list-ref lws 4)
-           ""))
+   'append-contexts rewrite-append-contexts
    (with-compound-rewriter
-    '~
-    (λ (lws) 
-      (list ""
-            (list-ref lws 2)
-            " ~ "
-            (list-ref lws 3)
-            ""))
-    (thunk))))
+    '~ rewrite-~
+    (with-compound-rewriter
+     'matches rewrite-matches
+     (with-compound-rewriter
+      'decomposes rewrite-decomposes
+      (thunk))))))
+
+(define (rewrite-append-contexts lws)
+  (list ""
+        (list-ref lws 2)
+        " ++ "
+        (list-ref lws 3)
+        " = "
+        (list-ref lws 4)
+        ""))
+
+(define (rewrite-~ lws)
+  (list ""
+        (list-ref lws 2)
+        " ~ "
+        (list-ref lws 3)
+        ""))
+
+(define (rewrite-matches lws)
+  (list ""
+        (list-ref lws 2)
+        " ⊢ "
+        (list-ref lws 3)
+        " : "
+        (list-ref lws 4)
+        " | "
+        (list-ref lws 5)))
+
+(define (rewrite-decomposes lws)
+  (list ""
+        (list-ref lws 2)
+        " ⊢ "
+        (list-ref lws 3)
+        " = "
+        (list-ref lws 4)
+        "["
+        (list-ref lws 5)
+        "]"
+        " : "
+        (list-ref lws 6)
+        " | "
+        (list-ref lws 7)))
