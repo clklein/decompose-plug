51648f399fa062d1b677e517256341d6c0ae5b3e
Robby Findler <robby@racket-lang.org>
2011-06-01 16:11:48 -0500

    adjusted primes

--- sem-sem/non-syntax-directed-match-define-relation.rkt
+++ sem-sem/non-syntax-directed-match-define-relation.rkt
@@ -8,9 +8,9 @@
 (define-relation patterns
   matches ⊆ L × t × p × b
   [(matches L a a (no-bindings))]
-  [(matches L t (:name x p) b_^’)
+  [(matches L t (:name x p) b_^′)
    (matches L t p b)
-   (lub-not-top (set (pair x t)) b b_^’)]
+   (lub-not-top (set (pair x t)) b b_^′)]
   [(matches L t (:nt n) (no-bindings))
    (nt-has-prod p L n)
    (matches L t p b)]
@@ -26,16 +26,16 @@
 (define-relation patterns
   decomposes ⊆ L × t × C × t × p × b
   [(decomposes L t :no-ctxt t :hole (no-bindings))]
-  [(decomposes L t_1 C t_2 (:name x p) b_^’)
+  [(decomposes L t_1 C t_2 (:name x p) b_^′)
    (decomposes L t_1 C t_2 p b)
-   (lub-not-top (set (pair x C)) b b_^’)]
-  [(decomposes L (:cons t_1 t_2) (:left t_2 C) t_1’ (:cons p_1 p_2) b)
-   (decomposes L t_1 C t_1’ p_1 b_1)
+   (lub-not-top (set (pair x C)) b b_^′)]
+  [(decomposes L (:cons t_1 t_2) (:left t_2 C) t_1^′ (:cons p_1 p_2) b)
+   (decomposes L t_1 C t_1^′ p_1 b_1)
    (matches L t_2 p_2 b_2)
    (lub-not-top b_1 b_2 b)]
-  [(decomposes L (:cons t_1 t_2) (:right t_1 C) t_1’ (:cons p_1 p_2) b)
+  [(decomposes L (:cons t_1 t_2) (:right t_1 C) t_1^′ (:cons p_1 p_2) b)
    (matches L t_1 p_1 b_1)
-   (decomposes L t_2 C t_2’ p_2 b_2)
+   (decomposes L t_2 C t_2^′ p_2 b_2)
    (lub-not-top b_1 b_2 b)]
   [(decomposes L t C t_2 (:in-hole p_1 p_2) b)
    (decomposes L t C_1 t_1 p_1 b_1)
