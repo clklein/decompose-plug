8a638bc1b0f71c77dd37220c01819eb850c90a29
Casey Klein <clklein@racket-lang.org>
2011-06-02 18:53:02 -0500

    Adds a forgotten file

---
+++ paper/typeset-reduction.rkt
@@ -0,0 +1,52 @@
+#lang racket
+
+(require redex/pict
+         slideshow/pict
+         "common.rkt"
+         "../sem-sem/common.rkt"
+         "../sem-sem/reduction.rkt")
+(provide render-reduction)
+
+(define (rewrite-reduces lws)
+  (list ""
+        (list-ref lws 2)
+        " ⊢ "
+        (list-ref lws 3)
+        " / "
+        (list-ref lws 4)
+        " → "
+        (list-ref lws 5)
+        " / "
+        (list-ref lws 6)
+        ""))
+
+(define (rewrite-meta-app lws)
+  (list "δ(" (list-ref lws 2) ", " (list-ref lws 3) ")"))
+
+(define compound-rewriters
+  (list (list 'meta-app rewrite-meta-app)
+        (list 'eq rewrite-eq)
+        (list 'set rewrite-set)
+        (list 'pair rewrite-pair)
+        (list 'matches rewrite-matches)
+        (list 'reduces rewrite-reduces)))
+
+(define (render-reduction)
+  (let loop ([rws compound-rewriters])
+    (match rws
+      ['() 
+       (with-keyword-rewriters
+        (λ ()
+          (vl-append
+           vertical-gap-size 
+           (hc-append
+            horizontal-gap-size
+            (render-relation reduces)
+            (parameterize ([render-language-nts '(r)])
+              (vl-append
+               (render-language reduction)
+               (non-bnf-def "f" (arbitrary-function-domain "t" "t")))))
+           (render-metafunctions inst plug)
+           (render-metafunction non-context))))]
+      [(cons (list name rewriter) rs)
+       (with-compound-rewriter name rewriter (loop rs))])))
